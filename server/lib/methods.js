a85f8e70186b35ea40dee9d976d9a6dec6a0fbee
Yonas Berhe <yonas.berhe007@gmail.com>
2016-01-01 17:39:02 -0800

    ref app on web app

--- server/lib/methods.js
+++ server/lib/methods.js
@@ -1,6 +1,35 @@
 'use strict';
 Meteor.methods({
-  addUser: function (user, onSuccess) {
+  addLeague: function(league, onSuccess) {
+    // Validations go here
+    // EmailDB._ensureIndex('email', {
+    //   unique: 1,
+    //   sparse: 1
+    // });
+
+
+    LeagueDB.insert({
+      _id: league.email,
+      firstName: league.firstName,
+      lastName: league.lastName,
+      phone: league.phone,
+      name: league.name,
+      registerDate: new Date(),
+    }, function(error, _id) {
+      if (_id !== null) {
+        if (error) {
+          if (error.code === 11000) {
+            return console.log("Duplicated Email");
+          }
+          // return console.log(error);
+        } else if (onSuccess) {
+          console.log('League info successfully submitted!');
+          onSuccess();
+        }
+      }
+    });
+  },
+  addUser: function(user, onSuccess) {
     // Validations go here
     // EmailDB._ensureIndex('email', {
     //   unique: 1,
@@ -10,18 +39,17 @@ Meteor.methods({
     EmailDB.insert({
       _id: user.email,
       signupDate: new Date(),
-    }, function (error, _id) {
-      if(_id !== null) {
-        if (error){
-          if (error.code === 11000){
+    }, function(error, _id) {
+      if (_id !== null) {
+        if (error) {
+          if (error.code === 11000) {
             return console.log("Duplicated Email");
           }
           // return console.log(error);
-        }
-        else if(onSuccess) {
+        } else if (onSuccess) {
           onSuccess();
         }
       }
     });
-  },
+  }
 });
